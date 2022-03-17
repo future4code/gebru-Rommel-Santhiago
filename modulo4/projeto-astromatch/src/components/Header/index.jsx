411b0236100fad58a1b8a2754f66fa08516e3253
rommelsanthiago <rommel.santhiago@gmail.com>
2022-03-17 13:56:27 -0300

    Iniciando projeto astromatch

---
+++ modulo4/projeto-astromatch/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header () {
+    return (
+        <>
+            <h1>astromatch</h1>
+            <button>matchs</button>
+        </>
+    )
+}
+
+export default Header
