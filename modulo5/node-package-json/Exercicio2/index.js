d016a34acef592694329f030c2d0af7e71931b10
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-19 18:02:35 -0300

    Otimização final

--- modulo5/node-package-json/Exercicio2/index.js
+++ modulo5/node-package-json/Exercicio2/index.js
@@ -6,8 +6,6 @@
 const colorize = require('colorize');
 const print = colorize.console;
 
-console.clear()
-
 const operation = process.argv[2]
 const num1 = Number(process.argv[3])
 const num2 = Number(process.argv[4])
@@ -15,15 +13,19 @@ const num2 = Number(process.argv[4])
 if(num1 && num2) {
     switch(operation){
         case "add":
+            print.log(`Soma de: #red[${num1} + ${num2}]`)
             print.log(`#bold[#green[Resposta:]] #red[${num1 + num2}]`)
             break;
         case "sub":
+            print.log(`Subtração de: #red[${num1} - ${num2}]`)
             print.log(`#bold[#green[Resposta:]] #red[${num1 - num2}]`)
             break;
         case "mult":
+            print.log(`Multiplicação de: #red[${num1} * ${num2}]`)
             print.log(`#bold[#green[Resposta:]] #red[${num1 * num2}]`)
             break;
         case "div":
+            print.log(`Divisão de: #red[${num1} ÷ ${num2}]`)
             print.log(`#bold[#green[Resposta:]] #red[${num1 / num2}]`)
             break;
     }
