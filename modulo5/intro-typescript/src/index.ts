1a83849efc08e9fc76ea4f6511a157c61fd2c7e2
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-24 21:18:22 -0300

    TypeScript - Exercicio 02

--- modulo5/intro-typescript/src/index.ts
+++ modulo5/intro-typescript/src/index.ts
@@ -1,8 +1,12 @@
 import checaTriangulo from './checaTriangulo'
+import coresFavoritas from './imprimeTresCoresFavoritas'
 
 const Exercicios = () => {
   // Exercicio 01
   console.log(checaTriangulo(10, 5, 5))
+
+  // Exercicio 02
+  coresFavoritas("Azul", "Branco", "Verde")
 }
 
 Exercicios()
