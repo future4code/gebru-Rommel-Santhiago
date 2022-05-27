2ad53a1ee279dc22f4132bb792dcdf827fe1b4c9
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-27 19:02:04 -0300

    Exercicio 02

--- modulo5/Projeto_Lista_Typescript/src/index.ts
+++ modulo5/Projeto_Lista_Typescript/src/index.ts
@@ -1,8 +1,15 @@
 import Exercicio01 from './Exercicio01'
+import Exercicio02 from './Exercicio02'
 
 const Exercicios = () => {
     console.log(Exercicio01('Rommel', '12/12/1987'))
     console.log(Exercicio01('Saullo', '12/01/2012'))
+
+    console.log(Exercicio02(1))
+    console.log(Exercicio02('1'))
+    console.log(Exercicio02(true))
+    console.log(Exercicio02([1, 2]))
+    console.log(Exercicio02({1: 1}))
 }
 
 Exercicios()
