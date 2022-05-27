ffd1918c2fd254f8c75b61fa3cb50b60f6e3e8cc
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-27 19:03:48 -0300

    Exercicio 04

--- modulo5/Projeto_Lista_Typescript/src/index.ts
+++ modulo5/Projeto_Lista_Typescript/src/index.ts
@@ -1,6 +1,7 @@
 import Exercicio01 from './Exercicio01'
 import Exercicio02 from './Exercicio02'
 import Exercicio03 from './Exercicio03'
+import Exercicio04 from './Exercicio04'
 
 enum GENERO {
 	ACAO="ação",
@@ -22,6 +23,19 @@ const Exercicios = () => {
 
     console.log(Exercicio03("Duna", 2021, GENERO.ACAO, 74))   
     console.log(Exercicio03("Duna", 2021, GENERO.ACAO))
+    
+    console.log(Exercicio04(
+            [
+                { nome: "Marcos", salário: 2500, setor: "marketing", presencial: true },
+                { nome: "Maria" ,salário: 1500, setor: "vendas", presencial: false},
+                { nome: "Salete" ,salário: 2200, setor: "financeiro", presencial: true},
+                { nome: "João" ,salário: 2800, setor: "marketing", presencial: false},
+                { nome: "Josué" ,salário: 5500, setor: "financeiro", presencial: true},
+                { nome: "Natalia" ,salário: 4700, setor: "vendas", presencial: true},
+                { nome: "Paola" ,salário: 3500, setor: "marketing", presencial: true }
+            ]
+        )
+    )
 }
 
 Exercicios()
