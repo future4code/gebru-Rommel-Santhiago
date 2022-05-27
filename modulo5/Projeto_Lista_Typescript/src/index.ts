6dd75bae2a2d15ded87c4d4297754f4d75215b44
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-27 19:04:26 -0300

    Exercicio 05

--- modulo5/Projeto_Lista_Typescript/src/index.ts
+++ modulo5/Projeto_Lista_Typescript/src/index.ts
@@ -2,6 +2,7 @@ import Exercicio01 from './Exercicio01'
 import Exercicio02 from './Exercicio02'
 import Exercicio03 from './Exercicio03'
 import Exercicio04 from './Exercicio04'
+import Exercicio05 from './Exercicio05'
 
 enum GENERO {
 	ACAO="ação",
@@ -36,6 +37,17 @@ const Exercicios = () => {
             ]
         )
     )
+
+    console.log(Exercicio05(
+        [
+            {name: "Rogério", email: "roger@email.com", role: "user"},
+            {name: "Ademir", email: "ademir@email.com", role: "admin"},
+            {name: "Aline", email: "aline@email.com", role: "user"},
+            {name: "Jéssica", email: "jessica@email.com", role: "user"},  
+            {name: "Adilson", email: "adilson@email.com", role: "user"},  
+            {name: "Carina", email: "carina@email.com", role: "admin"}      
+        ] 
+    ))
 }
 
 Exercicios()
