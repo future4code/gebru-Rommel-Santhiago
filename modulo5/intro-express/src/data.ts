b6ac53b400e2ee2ccb8c684b0763dd4d81493fae
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-31 16:16:37 -0300

    Exercicio 03

---
+++ modulo5/intro-express/src/data.ts
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "email": "Sincere@labenu.com",
+        "phone": 558888888801,
+        "website": "hildegard.org",
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "email": "ervin.howell@labenu.com",
+        "phone": 558888888802,
+        "website": "ervinhowell.com",
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "email": "clementine.bauch@labenu.com",
+        "phone": 558888888803,
+        "website": "clementinebauch.org",
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "email": "patricia.lebsack@labenu.com",
+        "phone": 558888888804,
+        "website": "patricialebsack.net",
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "email": "chelsey.dietrich@labenu.com",
+        "phone": 558888888805,
+        "website": "chelseydietrich.com",
+    }
+]
