84aaa4541c71ab44052846353751cc54fb4d361b
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-31 15:28:42 -0300

    Exercicio 01

---
+++ modulo5/intro-express/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req: Request , res: Response) => {
+    try {
+        res.status(200).send('Hello from Express')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(3003, () => {
+    console.log('Server is running in http://localhost:3003')
+})
