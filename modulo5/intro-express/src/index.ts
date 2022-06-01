4d6ab301ba6a610f71a90afb8cbdd9b73c25557e
rommelsanthiago <rommel.santhiago@gmail.com>
2022-05-31 22:20:35 -0300

    Exercicio 09 - Desafio

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -62,6 +62,30 @@ app.get('/posts/:id', (req: Request, res: Response) => {
     }
 })
 
+app.delete('/posts/:id', (req: Request, res: Response) => {
+    try {
+        const user: number = Number(req.params.id)
+        const postId: number = Number(req.query.id)
+        let newPosts: {}[] = []
+
+        for (let post of posts) {
+            if (post.userId === user && post.id === postId) {
+                // const index = post.userId && post.id
+                // if(index > -1){
+                //     newPosts = posts.slice(index, 1)
+                // }
+                newPosts = posts.filter((index) => {
+                    return index !== post
+                })
+            }
+        }
+        res.status(200).send(newPosts)
+    }
+    catch (error) {
+        res.status(400).end("Posts não encontrado")
+    }
+})
+
 app.listen(3003, () => {
     console.log('Server is running in http://localhost:3003')
 })
