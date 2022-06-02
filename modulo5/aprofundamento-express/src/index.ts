54df8e300eb8d2c90d5245548c82de1ea4ac44e2
rommelsanthiago <rommel.santhiago@gmail.com>
2022-06-02 15:30:45 -0300

    Exercicio 03

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from "cors"
 import { Request, Response } from "express";
+import { v4 as generateId } from 'uuid';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,39 @@ type ToDo = {
     "title": string,
     "completed": boolean
 }
+
+const toDos: ToDo = [
+    {
+        "userId": generateId(),
+        "id": generateId(),
+        "title": "delectus aut autem",
+        "completed": false
+      },
+      {
+        "userId": generateId(),
+        "id": generateId(),
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+      },
+      {
+        "userId": generateId(),
+        "id": generateId(),
+        "title": "fugiat veniam minus",
+        "completed": false
+      },
+      {
+        "userId": generateId(),
+        "id": generateId(),
+        "title": "et porro tempora",
+        "completed": true
+      },
+      {
+        "userId": generateId(),
+        "id": generateId(),
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+      }
+]
 
 app.get("/ping", (req: Request, res: Response) => {          
     res.send("Pong! 🏓")
