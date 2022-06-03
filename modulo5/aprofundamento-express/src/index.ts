ee516f4d55a76dc5350fbe681f6bda6debbbb268
rommelsanthiago <rommel.santhiago@gmail.com>
2022-06-02 22:16:09 -0300

    Exercicio 06

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -13,43 +13,66 @@ type ToDo = {
     "id": string,
     "title": string,
     "completed": boolean
-}[]
+}[];
 
 const toDos: ToDo = require("./toDos.json");
 
 app.get("/ping", (req: Request, res: Response) => {          
-    res.send("Pong! 🏓")
+    res.send("Pong! 🏓");
 })
 
 app.get("/status", (req: Request, res: Response) => {   
-    let toDosTrue: {}[] = []
-    const status = req.query.status === "true" ? true : false
+    let toDosTrue: {}[] = [];
+    const status = req.query.status === "true" ? true : false;
 
     for(let toDo of toDos){
         if(toDo.completed === status){
-            toDosTrue.push(toDo)
-        }
+            toDosTrue.push(toDo);
+        };
     }
-    res.status(200).send(toDosTrue)
+    res.status(200).send(toDosTrue);
 })
 
 app.post("/createToDo", (req: Request, res: Response) => {
-    const { title, completed } = req.body  
+    const { title, completed } = req.body;  
     const newToDo = {
         userId: generateId(),
         id: generateId(),
         title,
         completed
-    }       
+    };      
     
-    toDos.push(newToDo)
+    toDos.push(newToDo);
 
     fs.writeFile(__dirname + '/toDos.json', JSON.stringify(toDos, null, 4), (err) => {
         if (err) throw err;
-        res.status(201).send(toDos)
+        res.status(201).send(toDos);
+    });
+})
+
+app.put("/upDateToDo", (req: Request, res: Response) => {
+    const { userIdEdited, IdEdited} = req.query;
+    const { titleEdited, completedEdited } = req.body;
+
+    for(let toDo of toDos){
+        if(toDo.id === IdEdited){
+            const index = toDos.indexOf(toDo);
+            toDo = {
+                userId: (userIdEdited as string),
+                id: (IdEdited as string),
+                title: titleEdited,
+                completed: completedEdited
+            };
+            toDos[index] = toDo;
+        }
+    };
+
+    fs.writeFile(__dirname + '/toDos.json', JSON.stringify(toDos, null, 4), (err) => {
+        if (err) throw err;
+        res.status(201).send(toDos);
     });
 })
 
 app.listen(3003, () => {
-    console.log('Server running')
-})
+    console.log('Server running');
+});
