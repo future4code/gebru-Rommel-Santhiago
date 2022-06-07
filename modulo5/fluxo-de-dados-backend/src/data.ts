9453474f7f45a9e4bae10cb434da90b30435eea6
rommelsanthiago <rommel.santhiago@gmail.com>
2022-06-07 16:19:04 -0300

    Exercicio 02

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,29 @@
+import { v4 as generateId } from 'uuid'
+
+const produtos = [
+    {
+        id: generateId(),
+        name: "Notebook",
+        price: 3500
+    },
+    {
+        id: generateId(),
+        name: "Smart TV",
+        price: 3200
+    },
+    {
+        id: generateId(),
+        name: "Smartphone",
+        price: 2500
+    },
+    {
+        id: generateId(),
+        name: "Teclado Gamer",
+        price: 350
+    },
+    {
+        id: generateId(),
+        name: "headset gamer",
+        price: 200
+    }
+]
