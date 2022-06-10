0ec3f7b3b99f610a3a6b799dd5b6d00233c0c9ef
rommelsanthiago <rommel.santhiago@gmail.com>
2022-06-10 15:11:42 -0300

    Exercicio 07

--- modulo5/LabeBank/src/index.ts
+++ modulo5/LabeBank/src/index.ts
@@ -2,30 +2,38 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
 import { clientes } from "./data";
+import { idade } from "./calculaIdade"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.post('/clientes', (req: Request, res: Response) => {
+    let errorCode = 500;
     try {
       const { nome, cpf, dataNascimento } = req.body;
 
-      const newCliente = {
-        id: clientes.length + 1,
-        nome: nome,
-        cpf: cpf,
-        dataNascimento: dataNascimento,
-        saldo: 0,
-        extrato: []
-      };
+      if(idade(dataNascimento) >= 18){
+        const newCliente = {
+            id: clientes.length + 1,
+            nome: nome,
+            cpf: cpf,
+            dataNascimento: dataNascimento,
+            saldo: 0,
+            extrato: []
+        };
+    
+        clientes.push(newCliente);
 
-      clientes.push(newCliente);
+      } else {
+        errorCode = 422;
+        throw new Error("Cliente precisa ter mais de 18 anos para se cadastrar");
+      };
 
       res.status(201).send(clientes);
 
-    } catch (error) {
-      res.send(error);
+    } catch (error: any) {
+        res.status(errorCode).send(error.message);
     };
 });
 
