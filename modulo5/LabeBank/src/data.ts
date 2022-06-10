8e470b158611ec36696130f2e2ac8e75f7f67167
rommelsanthiago <rommel.santhiago@gmail.com>
2022-06-10 12:59:40 -0300

    Exercicio 02

--- modulo5/LabeBank/src/data.ts
+++ modulo5/LabeBank/src/data.ts
@@ -6,3 +6,14 @@ type Cliente = {
     saldo: number,
     extrato: {}[]
 }[]
+
+export const clientes: Cliente = [
+    {
+        id: 1,
+        nome: "Rommel",
+        cpf: 99999999999,
+        dataNascimento: "10/10/2000",
+        saldo: 2.000,
+        extrato: []
+    }
+]
