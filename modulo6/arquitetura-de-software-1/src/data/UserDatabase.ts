b421aca36a4238381307353ad43bcc83cb425719
rommelsanthiago <rommel.santhiago@gmail.com>
2022-07-26 16:19:09 -0300

    FEATURE: add module createUser

---
+++ modulo6/arquitetura-de-software-1/src/data/UserDatabase.ts
@@ -0,0 +1,17 @@
+import { BaseDatabase } from "./BaseDatabase";
+import { user } from "../types/user";
+
+export class UserDatabase extends BaseDatabase {
+   insertUser = async(user: user): Promise<void> => {
+    try {
+        await BaseDatabase.connection.insert({
+           id: user.id,
+           name: user.name,
+           email: user.email,
+           password: user.password
+        }).into('User_Arq')
+    } catch (error:any) {
+        throw new Error(error.message)
+    }
+   }
+}
