2b09176e11e1e2860ad35417f29a53f538ba22d2
rommelsanthiago <rommel.santhiago@gmail.com>
2022-07-28 20:13:06 -0300

    Fisrt commit - Arquitetura de software 2

---
+++ modulo6/arquitetura-software-2/src/business/UserBusiness.ts
@@ -0,0 +1,43 @@
+import { UserDatabase } from "../data/UserDatabase"
+import { v4 as generateId } from 'uuid';
+import { user } from "../types/user";
+
+export class UserBusiness {
+
+    createUSer = async (input:any):Promise<void> => {
+        try {
+            const {name, email, password} = input
+            if (
+                !name ||
+                !email ||
+                !password
+             ) {
+                throw new Error('Preencha os campos "name", "email" e "password"')
+             }
+             
+             const useDatabase = new UserDatabase()
+             await useDatabase.insertUser({
+                id: generateId(),
+                name,
+                email,
+                password
+             })
+        } catch (error:any) {
+            throw new Error(error.message)
+        }
+    }
+
+    async get(): Promise<user[]> {
+				
+        return await new UserDatabase().get();
+    }
+    
+    async deleteUser(input: {id:string}) {
+				
+        if(!input.id){
+            throw new Error("Insira um id!")
+        }
+
+        return await new UserDatabase().deleteUser(input.id);
+    }
+}
