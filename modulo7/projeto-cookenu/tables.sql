81f810ee5216939ed89b5bc29781317e1fc773e9
rommelsanthiago <rommel.santhiago@gmail.com>
2022-08-24 15:37:25 -0300

    Projeto Cookenu

---
+++ modulo7/projeto-cookenu/tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Cookenu_users (
+    id VARCHAR(64) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    role ENUM ("normal", "admin") NOT NULL DEFAULT "normal"
+);
