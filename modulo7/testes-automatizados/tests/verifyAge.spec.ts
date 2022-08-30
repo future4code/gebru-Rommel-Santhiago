da9e5c584d051952432f715d6f83fac5bf1de4ce
rommelsanthiago <rommel.santhiago@gmail.com>
2022-08-30 19:35:22 -0300

    FEAT: Challenge tests: verify age of 1 American allowed

--- modulo7/testes-automatizados/tests/verifyAge.spec.ts
+++ modulo7/testes-automatizados/tests/verifyAge.spec.ts
@@ -16,3 +16,19 @@ test("1 brazilian allowed", () => {
     const result = verifyAge(casino, [brazilian]);
     expect(result.brazilians.allowed).toEqual(["Astrodev"]);
 });
+
+test("1 american allowed", () => {
+    const brazilian: User = {
+        name: "Astrodev",
+        age: 19,
+        nacionality: NACIONALITY.AMERICAN,
+    };
+
+    const casino: Casino = {
+        name: "Balada Estelar",
+        location: LOCATION.BRAZIL,
+    };
+
+    const result = verifyAge(casino, [brazilian]);
+    expect(result.americans.allowed).toEqual(["Astrodev"]);
+});
